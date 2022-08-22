57592895927f6419d1964f329d1f95668d6377e9
WaltonG <waltongoga@gmail.com>
2022-08-22 21:59:16 +0300

    Update deleteTodo.ts

--- serverless-application-project/backend/src/lambda/http/deleteTodo.ts
+++ serverless-application-project/backend/src/lambda/http/deleteTodo.ts
@@ -1,25 +1,31 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { deleteTodo } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
 
-export const handler = middy(
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('deleteTodo')
+
+export const handler: APIGatewayProxyHandler = 
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Remove a TODO item by id
+    logger.info("Deleting Todo")
     
-    return undefined
+    const userId = getUserId(event)
+    const deleteItem = await deleteTodo(todoId, userId)
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+        "item": deleteItem
+      })
+    }
   }
-)
 
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
-    })
-  )
