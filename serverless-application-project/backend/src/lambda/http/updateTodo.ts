383e74b5bc42bab83726b884e837d225b5d9630f
WaltonG <waltongoga@gmail.com>
2022-08-22 21:59:24 +0300

    Update updateTodo.ts

--- serverless-application-project/backend/src/lambda/http/updateTodo.ts
+++ serverless-application-project/backend/src/lambda/http/updateTodo.ts
@@ -1,27 +1,34 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+// import * as middy from 'middy'
+// import { cors, httpErrorHandler } from 'middy/middlewares'
 
 import { updateTodo } from '../../businessLogic/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
 
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+const logger = createLogger('updateTodo')
 
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  logger.info('updateTodo event')
 
-    return undefined
-)
-
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
+  const userId = getUserId(event)
+  const updatedItem = await updateTodo(todoId, updatedTodo, userId)
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      "item": updatedItem
     })
-  )
+  }
+
+}
