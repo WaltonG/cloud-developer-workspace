14b6deeee69a04d523f758d152eba9e4e650976a
WaltonG <waltongoga@gmail.com>
2022-08-22 21:41:22 +0300

    Update createTodo.ts

--- serverless-application-project/backend/src/lambda/http/createTodo.ts
+++ serverless-application-project/backend/src/lambda/http/createTodo.ts
@@ -1,21 +1,28 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 import { createTodo } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
 
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const logger = createLogger('createTodo')
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
+    const userId = getUserId(event)
+    logger.info(`Creating a new TODO item for ${userId}`)
 
-    return undefined
-)
+    const newItem = await createTodo(newTodo, userId)
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+        "item": newItem
+      })
+    }
+  }
